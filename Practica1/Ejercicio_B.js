4b7fa93aee820627b15b8f2739005b2e4b2e1b37
ser180 <sersotoolm@gmail.com>
2025-05-16 19:16:09 -0600

    Ejercicio B
    Completo

---
+++ Practica1/Ejercicio_B.js
@@ -0,0 +1,6 @@
+/*b. Convierte esta función tradicional a una arrow function que haga exactamente lo
+mismo:
+Probar usando 3 numeros distintos*/
+
+let cuadrado = (num) => num * num;
+console.log(cuadrado(11))
