459612e7174c86936f16f3ceb6dd2774a822c692
ser180 <sersotoolm@gmail.com>
2025-05-16 19:30:29 -0600

    Ejercicio C
    Completo

---
+++ Practica1/Ejercicio_C.js
@@ -0,0 +1,8 @@
+/*Crea una arrow fuction llamada saludoPersonalizado que reciba dos parametros: nombre y 
+edad, y retorne una cadena como la siguiente */
+//"Hola, me llamo Sergio y tengo 20 años."
+
+const saludoPersonalizado = (nombre,edad) => "Hola, me llamo " + nombre + " y tengo " + edad + " años";
+
+console.log(saludoPersonalizado("Sergio", 20));
+
