fbdf601f66b6a9fc4beb1792c35dd33af75b1539
ser180 <sersotoolm@gmail.com>
2025-06-13 14:30:56 -0600

    Componentes Texto
    Componentes Texto propio con su propiedad Contenido

--- EJblank/App.js
+++ EJblank/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
- const Texto= ()=>{ //Hereda todo lo que tiene el teto
+ const Texto= (props)=>{ //Hereda todo lo que tiene el teto, Realizar el componente dinaminco
+  const {contenido}=props 
     return(
-      <Text> Hola Mundo desde React Native</Text>
+      <Text>{contenido}</Text> // Lo que se pase a traves de contenido aparecera en el texto
     )
   }
 
@@ -11,12 +12,14 @@ export default function App() { //Ejecuta o crea la vista del proyecto
   return (
 
     <View style={styles.container}>
-      
-      <Texto></Texto>
       <StatusBar style="auto" />
-      <Texto></Texto>
+      
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="mundo"></Texto>
+      <Texto contenido="React Native"></Texto>
+      
       <Button title="Touch me" />
-      <Texto></Texto>
+      
 
     </View>
   );
