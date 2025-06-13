ebf44a64071cc0787fc30bef54bda2be14dc68da
ser180 <sersotoolm@gmail.com>
2025-06-13 14:39:33 -0600

    Children
    Componentes

--- EJblank/App.js
+++ EJblank/App.js
@@ -2,9 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
  const Texto= (props)=>{ //Hereda todo lo que tiene el teto, Realizar el componente dinaminco
-  const {contenido}=props 
+  const {children}=props 
     return(
-      <Text>{contenido}</Text> // Lo que se pase a traves de contenido aparecera en el texto
+      <Text>{children}</Text> // Lo que se pase a traves de contenido aparecera en el texto
     )
   }
 
@@ -14,9 +14,9 @@ export default function App() { //Ejecuta o crea la vista del proyecto
     <View style={styles.container}>
       <StatusBar style="auto" />
       
-      <Texto contenido="Hola"></Texto>
-      <Texto contenido="mundo"></Texto>
-      <Texto contenido="React Native"></Texto>
+      <Texto>"Hola"</Texto>
+      <Texto>"Mundo"</Texto>
+      <Texto>React Native</Texto>
       
       <Button title="Touch me" />
       
